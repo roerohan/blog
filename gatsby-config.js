0c9630f5ecfb1115cf9d1978592095cab7a263a6
Rohan Mukherjee <roerohan@gmail.com>
2023-10-02 18:46:58 +0530

    fix: update logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,15 +116,15 @@ Here are some articles I've written in the past on <a href="https://medium.com/@
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `Nerd's Notebook`,
+        short_name: `Nerd's Notebook`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
   ],
